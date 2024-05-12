845fa43a84a23f40437e6baece5c4115de16379b
Bilal Sheikh <72bilal.sheikh@gmail.com>
2024-05-12 17:10:22 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Vimit Mobile Chat App',
+  webDir: 'dist'
+};
+
+export default config;
